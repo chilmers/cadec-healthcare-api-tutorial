f1efa2e0dd0a401b59f40e8428e6780a6208944d
Christian Hilmersson <christian.hilmersson@gmail.com>
2012-12-11 15:21:30 +0100

    Added project structure for backend

---
+++ backend/src/main/webapp/resources/assets/base.js
@@ -0,0 +1,6 @@
+(function( window ) {
+	'use strict';
+	
+	console.log('Running base.js');
+	
+})( window );
---
+++ backend/src/main/webapp/resources/js/views/app.js
@@ -0,0 +1,23 @@
+var app = app || {};
+
+$(function( $ ) {
+	'use strict';
+	
+	app.AppView = BackBone.View.Extend({
+		
+		events: {
+			
+		},
+		
+		initialize: function() {
+			
+		},
+		
+		render: function() {
+			
+		}
+		
+	});
+	
+	
+});
