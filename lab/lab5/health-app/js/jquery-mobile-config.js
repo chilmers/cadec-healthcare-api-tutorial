27db27380219bb5a1d647dfeea1460c073b2c394
Christian Hilmersson <christian.hilmersson@gmail.com>
2013-01-09 15:46:38 +0100

    Removed unused config

--- lab/lab5/health-app/js/jquery-mobile-config.js
+++ lab/lab5/health-app/js/jquery-mobile-config.js
@@ -2,7 +2,6 @@ var app = app || {};
 $(document).bind("mobileinit", function () {
 	// Turn off JQuery Mobile's link handling since we want to use Backbone for rendering
 	// See: http://jquerymobile.com/test/docs/pages/backbone-require.html
-//    $.mobile.ajaxEnabled = false;
     $.mobile.linkBindingEnabled = false;
     $.mobile.hashListeningEnabled = false;
     $.mobile.pushStateEnabled = false;
